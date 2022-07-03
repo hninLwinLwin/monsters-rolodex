b3af8bb0fca1c65e40f9ffe81153bda1712fd851
HLL <snow@inspiralcreative.com>
2022-07-03 16:40:15 +0630

    git page

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,6 +3,7 @@ import "./card.style.css"
 export const Card = (props) => (
 		<div className="card-container">
 			<img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set2`} />
-			<h1> {props.monster.name}</h1>
+			<h2> {props.monster.name}</h2>
+			<p> {props.monster.email}</p>
 		</div>
 	)
