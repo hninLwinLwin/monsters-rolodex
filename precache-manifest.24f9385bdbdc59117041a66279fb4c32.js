4c53688bf2a8e385520bb5e8f8e780b86eb68544
HLL <snow@inspiralcreative.com>
2022-07-03 16:38:56 +0630

    Updates

---
+++ precache-manifest.24f9385bdbdc59117041a66279fb4c32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "592118cfaeb3512839e3cc735632f0ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c670d7f750f0031d016",
+    "url": "/static/css/main.616f7942.chunk.css"
+  },
+  {
+    "revision": "c3b468415663d126bfa0",
+    "url": "/static/js/2.e6d309bb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.e6d309bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c670d7f750f0031d016",
+    "url": "/static/js/main.c9140a2f.chunk.js"
+  },
+  {
+    "revision": "9152199139774ad37345",
+    "url": "/static/js/runtime-main.8b5813b6.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  }
+]);
